4e09084e01729dd98395527d11e21cbf19968296
ryancooper7 <ryan.cooper7@gmail.com>
2018-04-01 19:45:29 -0500

    Added a button to demonstrate the front-end is connected to the back end. Sends a number to the back-end with HTML and then computes the square root on the back end and returns the result to the user on the page.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+import math
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/<string:index>/')
 def render_static(index):
     return render_template('%s.html' % index)
+
+@app.route('/<string:index>/', methods=['POST'])
+def my_form_post(index):
+    text = float(request.form['text'])
+    processed_text = str(math.sqrt(text))
+    return render_template('%s.html' % index, text=processed_text)
 
 
 if __name__ == "__main__":
