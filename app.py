26913f9cd30bd9287f40a524ec1b3fd179bb7d1f
ryancooper7 <ryan.cooper7@gmail.com>
2018-04-17 14:22:13 -0600

    Sound data recorded with HTML can now be sent to back-end

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/<string:index>/')
-def render_static(index):
-    return render_template('%s.html' % index)
-
-@app.route('/<string:index>/', methods=['POST'])
+@app.route('/<string:index>/', methods=['GET','POST'])
 def my_form_post(index):
-    text = float(request.form['text'])
-    processed_text = str(math.sqrt(text))
-    return render_template('%s.html' % index, text=processed_text)
-
+    if request.method == 'POST':
+        print int(request.data)
+        return request.data
+    else:
+        return render_template('%s.html' % index)
 
 if __name__ == "__main__":
     app.run(debug=True)
